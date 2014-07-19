5a9a38277fd9ef7020d06f8bd85955a82304e48b
Teuku Dani <teuku.dani23@gmail.com>
2014-07-19 15:38:46 +0700

    alert view when login

--- bioRhythm/Classes/Controller/RBLoginViewController.m
+++ bioRhythm/Classes/Controller/RBLoginViewController.m
@@ -11,6 +11,10 @@
 @interface RBLoginViewController ()
 @property (strong, nonatomic) IBOutlet UIButton *buttonLogin;
 @property (strong, nonatomic) IBOutlet UILabel *labelWelcome;
+@property (strong, nonatomic) IBOutlet UITextField *textFieldName;
+@property (strong, nonatomic) IBOutlet UITextField *textFieldPassword;
+
+
 - (IBAction)buttonLoginPressed:(id)sender;
 
 @end
@@ -23,6 +27,7 @@
     [super viewDidLoad];
     
     self.buttonLogin.backgroundColor = [UIColor blueColor];
+    
     // Do any additional setup after loading the view.
 }
 
@@ -33,18 +38,26 @@
 }
 
 /*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+ {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 #pragma mark - action
 - (IBAction)buttonLoginPressed:(id)sender {
-    _labelWelcome.text = @"berhasil";
-    
+    NSString *name = _textFieldName.text;
+    NSString *password = _textFieldPassword.text;
+    UIAlertView *alertView;
+    if (name.length == 0 || password.length == 0) {
+        alertView  = [[UIAlertView alloc] initWithTitle:@"error" message:@"please fill username and password" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+        [alertView show];
+    } else {
+        alertView = [[UIAlertView alloc] initWithTitle:@"login" message:@"succed" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+        [alertView show];
+    }
 }
 @end
