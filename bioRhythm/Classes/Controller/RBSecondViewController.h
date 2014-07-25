4ce596cc4825899a11c625e3d383c8866223563a
Teuku Dani <teuku.dani23@gmail.com>
2014-07-25 14:09:56 +0700

    second view controller added

---
+++ bioRhythm/Classes/Controller/RBSecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  RBSecondViewController.h
+//  bioRhythm
+//
+//  Created by defindal irvan on 7/25/14.
+//  Copyright (c) 2014 defindal irvan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RBSecondViewController : UIViewController
+
+@end
---
+++ bioRhythm/Classes/Controller/RBSecondViewController.m
@@ -0,0 +1,56 @@
+//
+//  RBSecondViewController.m
+//  bioRhythm
+//
+//  Created by defindal irvan on 7/25/14.
+//  Copyright (c) 2014 defindal irvan. All rights reserved.
+//
+
+#import "RBSecondViewController.h"
+
+#import "UIViewController+ECSlidingViewController.h"
+
+@interface RBSecondViewController ()
+
+@end
+
+@implementation RBSecondViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - action
+- (IBAction)buttonMenuPressed:(id)sender {
+    [self.slidingViewController anchorTopViewToRightAnimated:YES];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
